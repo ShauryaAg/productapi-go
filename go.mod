a4e0219b17038986eb10cf1421eac895ffc62dec
Shaurya <agarwal.lucky.shaurya11@gmail.com>
2022-01-27 23:32:26 +0530

    feat (models): define `user` model

--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/go-chi/chi/v5 v5.0.7
 	github.com/rs/cors v1.8.2
+	go.mongodb.org/mongo-driver v1.8.2
 	golang.org/x/crypto v0.0.0-20220126234351-aa10faf2a1f8
 )
---
+++ models/user.go
@@ -0,0 +1,42 @@
+package models
+
+import (
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	Id       primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Name     string             `json:"name" bson:"name"`
+	Password string             `json:"password" bson:"password"`
+	Email    string             `json:"email" bson:"email"`
+	IsActive bool               `json:"is_active" bson:"is_active"`
+}
+
+func (u *User) HashPassword() error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	u.Password = string(hash)
+	return nil
+}
+
+func (u *User) VerifyPassword(attempt string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(attempt))
+	if err != nil {
+		fmt.Println(err)
+		return false
+	}
+
+	return true
+}
+
+func (u *User) SetPassword(new string) {
+	u.Password = new
+	u.HashPassword()
+}
