441562c160bd6b8bbfc2ac8a2245768eea3cace9
Shaurya <agarwal.lucky.shaurya11@gmail.com>
2022-01-31 22:29:34 +0530

    fix (utils): correctly returning error from `utils.ParseToken`

--- utils/jwt.go
+++ utils/jwt.go
@@ -34,12 +34,14 @@ func ParseToken(tokenString string) (*jwt.MapClaims, error) {
 	token, err := jwt.ParseWithClaims(tokenString, &jwt.MapClaims{}, func(token *jwt.Token) (interface{}, error) {
 		return []byte(secret), nil
 	})
+	if err != nil {
+		return nil, err
+	}
 
 	if claims, ok := token.Claims.(*jwt.MapClaims); ok && token.Valid {
 		return claims, err
-	} else {
-		fmt.Println(err)
 	}
 
+	fmt.Println("err", err)
 	return nil, err
 }
